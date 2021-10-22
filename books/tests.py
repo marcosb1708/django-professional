7e42b7c965aff657c08b69b4b070236dfe6037b0
marcos <marcos.b1708@gmail.com>
2021-10-21 22:26:49 -0400

    ch13

--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import client,TestCase
 from django.urls import reverse
 from .models import Book,Reveiew
@@ -7,21 +8,21 @@ class BookTests(TestCase):
 
     def setUp(self):
         self.user = get_user_model().objects.create_user(
-            username = 'reviewuser',
-            email = 'reviewuser@email.com',
-            password = 'testpass123',
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
         )
-        
+        self.special_permission = Permission.objects.get(
+            codename='special_status') 
         self.book = Book.objects.create(
-            title ='Harry Potter',
+            title='Harry Potter',
             author='JK Rowling',
             price='25.00',
         )
-    
         self.review = Reveiew.objects.create(
             book = self.book,
             author = self.user,
-            review = 'an excellent review',
+            review = 'An excellent review',
         )
 
     def test_book_listing(self):
@@ -30,17 +31,30 @@ class BookTests(TestCase):
         self.assertEqual(f'{self.book.price}','25.00')
 
 
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_user(self): 
+        self.client.login(email='reviewuser@email.com', password='testpass123')
         response = self.client.get(reverse('book_list'))
-        self.assertEqual(response.status_code,200)
-        self.assertContains(response,'Harry Potter')
-        self.assertTemplateUsed(response,'books/book_list.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Harry Potter')
+        self.assertTemplateUsed(response, 'books/book_list.html')
 
-    def test_book_detal_view(self):
+    def test_book_list_view_for_logged_out_user(self):  
+        self.client.logout()
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, '%s?next=/books/' % (reverse('account_login')))
+        response = self.client.get(
+            '%s?next=/books/' % (reverse('account_login')))
+        self.assertContains(response, 'Log In')
+
+    def test_book_detail_view_with_permissions(self): 
+        self.client.login(email='reviewuser@email.com', password='testpass123')
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
-        no_response = self.client.get('/books/12345')
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(no_response.status_code,404)
-        self.assertContains(response,'Harry Potter')
-        self.assertContains(response, 'an excellent review')
-        self.assertTemplateUsed(response,'books/book_detail.html')
+        no_response = self.client.get('/books/12345/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'An excellent review')
+        self.assertTemplateUsed(response, 'books/book_detail.html')
--- books/views.py
+++ books/views.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.views.generic import ListView,DetailView
 from .models import Book
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin,ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+    permission_required = 'books.special_status'
