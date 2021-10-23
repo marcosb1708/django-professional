ecf65218dcf87eaec01976da3c2b169aee99f5a3
marcos <marcos.b1708@gmail.com>
2021-10-23 10:00:02 -0400

    docker-compose-prod

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+ENVIRONMENT = os.environ.get('ENVIRONMENT')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG',default=0))
 
